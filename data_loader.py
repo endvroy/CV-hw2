cf49fea17e0ad9e543cf935c073d06fd31bf40fb
ruoyi <endvroy@gmail.com>
2019-10-27 17:41:53 -0400

    bug fixes and now pins memory

--- data_loader.py
+++ data_loader.py
@@ -1,21 +1,31 @@
 import torch
-from torchvision import datasets
+from torchvision import datasets, transforms
+
 import os
+
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+])
 
 
 def load_train(data_path, batch_size):
     train_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(os.path.join(data_path, 'train_images')),
+        datasets.ImageFolder(os.path.join(data_path, 'train_images'),
+                             transform=transform),
         batch_size=batch_size,
         shuffle=True,
+        pin_memory=True,
         num_workers=1)
     return train_loader
 
 
 def load_val(data_path, batch_size):
     val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(os.path.join(data_path, 'val_images')),
+        datasets.ImageFolder(os.path.join(data_path, 'val_images'),
+                             transform=transform),
         batch_size=batch_size,
         shuffle=False,
+        pin_memory=True,
         num_workers=1)
     return val_loader
