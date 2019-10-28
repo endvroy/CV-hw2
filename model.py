66f5bca8c6734de9cb786740c0b168fc8efb802a
ruoyi <endvroy@gmail.com>
2019-10-28 14:28:57 -0400

    wrapped STN into standalone module

--- model.py
+++ model.py
@@ -5,15 +5,9 @@ import torch.nn.functional as F
 nclasses = 43  # GTSRB as 43 classes
 
 
-class Net(nn.Module):
+class STN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(500, 50)
-        self.fc2 = nn.Linear(50, nclasses)
-
+        super(STN, self).__init__()
         self.localization = nn.Sequential(
             nn.Conv2d(3, 8, kernel_size=7),
             nn.MaxPool2d(2, stride=2),
@@ -34,16 +28,26 @@ class Net(nn.Module):
         self.fc_loc[2].weight.data.zero_()
         self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
 
-    def stn(self, x):
+    def forward(self, x):
         xs = self.localization(x)
         xs = xs.view(-1, 160)
         theta = self.fc_loc(xs)
         theta = theta.view(-1, 2, 3)
-
-        grid = F.affine_grid(theta, x.size())
-        x = F.grid_sample(x, grid)
+        grid = F.affine_grid(theta, x.size(), align_corners=True)
+        x = F.grid_sample(x, grid, align_corners=True)
 
         return x
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(500, 50)
+        self.fc2 = nn.Linear(50, nclasses)
+        self.stn = STN()
 
     def forward(self, x):
         # transform the input
