465333cae98ebb90085acde4f587cb49436be181
ruoyi <endvroy@gmail.com>
2019-10-28 15:22:21 -0400

    wrapped the code in my style

--- model.py
+++ model.py
@@ -6,32 +6,30 @@ nclasses = 43  # GTSRB as 43 classes
 
 
 class STN(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels):
         super(STN, self).__init__()
-        self.localization = nn.Sequential(
-            nn.Conv2d(3, 8, kernel_size=7),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True),
-            nn.Conv2d(8, 10, kernel_size=5),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True)
-        )
-
-        # Regressor for the 3 * 2 affine matrix
-        self.fc_loc = nn.Sequential(
-            nn.Linear(160, 32),
-            nn.ReLU(True),
-            nn.Linear(32, 3 * 2)
-        )
+        self.conv1 = nn.Conv2d(in_channels, 8, kernel_size=7)
+        self.pool1 = nn.MaxPool2d(2, stride=2)
+        self.conv2 = nn.Conv2d(8, 10, kernel_size=5)
+        self.pool2 = nn.MaxPool2d(2, stride=2)
+        self.fc1 = nn.Linear(160, 32)
+        self.fc2 = nn.Linear(32, 3 * 2)
 
         # Initialize the weights/bias with identity transformation
-        self.fc_loc[2].weight.data.zero_()
-        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+        self.fc2.weight.data.zero_()
+        self.fc2.bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
 
     def forward(self, x):
-        xs = self.localization(x)
+        xs = self.conv1(x)
+        xs = self.pool1(xs)
+        xs = F.leaky_relu(xs, inplace=True)
+        xs = self.conv2(xs)
+        xs = self.pool2(xs)
+        xs = F.leaky_relu(xs, inplace=True)
         xs = xs.view(-1, 160)
-        theta = self.fc_loc(xs)
+        xs = self.fc1(xs)
+        xs = F.leaky_relu(xs, inplace=True)
+        theta = self.fc2(xs)
         theta = theta.view(-1, 2, 3)
         grid = F.affine_grid(theta, x.size(), align_corners=True)
         x = F.grid_sample(x, grid, align_corners=True)
@@ -43,18 +41,25 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.pool1 = nn.MaxPool2d(2)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
+        self.pool2 = nn.MaxPool2d(2)
         self.fc1 = nn.Linear(500, 50)
         self.fc2 = nn.Linear(50, nclasses)
-        self.stn = STN()
+        self.stn = STN(3)
 
     def forward(self, x):
-        # transform the input
         x = self.stn(x)
 
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = self.conv2_drop(x)
+        x = self.pool2(x)
+        x = F.relu(x)
+
         x = x.view(-1, 500)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
