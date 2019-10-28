057c63368379e2deb6aab5fab9e0bff5658aa2cd
ruoyi <endvroy@gmail.com>
2019-10-27 23:23:25 -0400

    now shouldn't create empty folders in val set

--- data_prep.py
+++ data_prep.py
@@ -20,10 +20,10 @@ def make_val_ds(data_path, val_ratio=0.2):
     train_path = os.path.join(data_path, 'train_images')
     val_path = os.path.join(data_path, 'val_images')
     for cat in tqdm(os.listdir(train_path)):
-        val_cat_path = os.path.join(val_path, cat)  # the path to copy val data to
-        os.makedirs(val_cat_path)  # make dir
         train_cat_path = os.path.join(train_path, cat)
         if os.path.isdir(train_cat_path):
+            val_cat_path = os.path.join(val_path, cat)  # the path to copy val data to
+            os.makedirs(val_cat_path)  # make dir
             train_f_list = os.listdir(train_cat_path)  # train data in category cat
             val_n = int(len(train_f_list) * val_ratio)  # calc n of val images
             # randomly pick val files
