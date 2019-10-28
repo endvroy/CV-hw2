c039aa7361201fd1086456bd1955c8abc9059b4f
ruoyi <endvroy@gmail.com>
2019-10-28 01:32:29 -0400

    added train logger

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
+import operator
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
 import data_loader
 from model import Net
-import os
 from tqdm import tqdm
 
 params = {
@@ -11,12 +12,36 @@ params = {
     'batch_size': 256,
     'lr': 1e-3,
     'momentum': 1e-2,
-    'log_interval': 2,
-    'epoches': 30
+    'log_interval': 10,
+    'epoches': 3
 }
 
 
-def train_epoch(model, optimizer, train_loader, epoch):
+class TrainLogger:
+    def __init__(self):
+        self.epoches = {}
+        self.vals = {}
+
+    def log(self, epoch, batch, loss):
+        if epoch not in self.epoches:
+            self.epoches[epoch] = {}
+        self.epoches[epoch][batch] = loss
+
+    def log_val(self, epoch, loss, accuracy):
+        self.vals[epoch] = [loss, accuracy]
+
+    def n_epoches(self):
+        return len(self.epoches)
+
+    def epoch_losses(self):
+        losses = []
+        for epoch in range(1, self.n_epoches() + 1):
+            loss = max(self.epoches[epoch].items(), key=operator.itemgetter(0))[1]
+            losses.append(loss)
+        return losses
+
+
+def train_epoch(model, optimizer, train_loader, epoch, train_logger):
     model.train()
     for batch_idx, (inp, target) in enumerate(train_loader):
         inp, target = inp.cuda(non_blocking=True), target.cuda(non_blocking=True)
@@ -26,6 +51,7 @@ def train_epoch(model, optimizer, train_loader, epoch):
         loss.backward()
         optimizer.step()
         if batch_idx % params['log_interval'] == 0:
+            train_logger.log(epoch, batch_idx, loss.item())
             print('Epoch {}, Batch {}: [{}/{} ({:.0f}%)]    Loss: {:.6f}'.format(
                 epoch,
                 batch_idx,
@@ -35,7 +61,7 @@ def train_epoch(model, optimizer, train_loader, epoch):
                 loss.item()))
 
 
-def validate(model, val_loader, epoch):
+def validate(model, val_loader, epoch, train_logger):
     print(f'Validating epoch {epoch}:')
     model.eval()
     val_loss = 0
@@ -49,44 +75,53 @@ def validate(model, val_loader, epoch):
         correct += pred.eq(target.data.view_as(pred)).sum()
 
     val_loss /= len(val_loader.dataset)
-    print('Validation loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    accuracy = 100. * correct.item() / len(val_loader.dataset)
+    train_logger.log_val(epoch, val_loss, accuracy)
+    print('Validation loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
         val_loss,
         correct,
         len(val_loader.dataset),
         100. * correct / len(val_loader.dataset)))
 
 
-def save_checkpoint(model, optimizer, checkpoint_path, epoch, continued=False):
-    os.makedirs(checkpoint_path, exist_ok=continued)
+def save_checkpoint(model, optimizer, checkpoint_path, epoch, train_logger):
+    os.makedirs(checkpoint_path, exist_ok=True)
     state = {'model': model.state_dict(),
              'optimizer': optimizer.state_dict(),
-             'epoch': epoch}
+             'epoch': epoch,
+             'train_logger': train_logger}
     fname = f'epoch_{epoch}.pth'
-    torch.save(state, os.path.join(checkpoint_path, fname))
+    fpath = os.path.join(checkpoint_path, fname)
+    torch.save(state, fpath)
+    print(f'checkpoint saved to {fpath}')
 
 
 def load_check_point(model, optimizer, checkpoint_path):
     state = torch.load(checkpoint_path)
     model.load_state_dict(state['model'])
     optimizer.load_state_dict(state['optimizer'])
-    return state['epoch']
+    return state['epoch'], state['train_logger']
 
 
-def train(model, optimizer, epoch, epoches_to_train, save_path, continued=False):
+def train(model, optimizer, epoch, epoches_to_train, save_path, train_logger):
     for epoch_i in range(1, epoches_to_train + 1):
-        train_epoch(model, optimizer, train_loader, epoch + epoch_i)
-        validate(model, val_loader, epoch + epoch_i)
-        save_checkpoint(model, optimizer, save_path, epoch + epoch_i, continued)
+        train_epoch(model, optimizer, train_loader, epoch + epoch_i, train_logger)
+        validate(model, val_loader, epoch + epoch_i, train_logger)
+        save_checkpoint(model, optimizer, save_path, epoch + epoch_i, train_logger)
+        print('')
 
 
 if __name__ == '__main__':
     train_loader = data_loader.load_train(params['data_path'], params['batch_size'])
     val_loader = data_loader.load_val(params['data_path'], params['batch_size'])
+    print('train and val data loaded')
+
     model = Net()
     model.cuda()
     optimizer = optim.Adam(model.parameters(), lr=params['lr'])
-    # epoch = 0
-    # epoches_to_train = 10
-    epoch = load_check_point(model, optimizer, 'checkpoints_t/epoch_10.pth')
+    epoch = 0
     epoches_to_train = params['epoches']
-    train(model, optimizer, epoch, epoches_to_train, 'checkpoints_t', continued=True)
+    train_logger = TrainLogger()
+    # epoch, train_logger = load_check_point(model, optimizer, 'checkpoints_t/epoch_10.pth')
+    # epoches_to_train = 10
+    train(model, optimizer, epoch, epoches_to_train, 'checkpoints/1', train_logger)
