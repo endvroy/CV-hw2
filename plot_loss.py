4dbd606b5b00a2234466454133e2c5939579129e
ruoyi <endvroy@gmail.com>
2019-10-28 23:35:16 -0400

    now plots the new best model

--- plot_loss.py
+++ plot_loss.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from utils import load_state, load_train_logger
 
 if __name__ == '__main__':
-    state = load_state('checkpoints/stn6/epoch_20.pth')
+    state = load_state('checkpoints/stn7/epoch_20.pth')
     train_logger = load_train_logger(state)
     losses = train_logger.epoch_losses()
     epoches = range(1, train_logger.n_epoches() + 1)
